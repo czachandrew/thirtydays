b2308942106c2e0f96f41e9f9d8691a25ae3bb7e
Andrew Czachowski <czachandrew@gmail.com>
2020-03-18 16:37:29 -0500

    adding guraded to krate model

--- app/Krate.php
+++ app/Krate.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Krate extends Model
 {
     //
+    protected $guarded = [];
 
     public function kratespace(){
         return $this->belongsTo('App\Kratespace');
